0f98562c682691367f3ee0923c083ca3dae1cc69
meindertkroese <>
2024-07-15 15:58:52 +0200

    get request

--- scripts/server.py
+++ scripts/server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/clarin/cmd/mapping/check", methods=['GET'])
 #  localhost/clarin/cmd/mapping/check?prof=<id>
 def check():
-    return render_template('index.html') #, person=name)
+    profile = request.args.get('prof', None)
+    return f"<b>{profile}</b>"
+#    return render_template('index.html', profile=profile)
 
 @app.route("/")
 def hello_world():
