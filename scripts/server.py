8696eae5ac66d1ec2b7d3593939ceb9430eaeada
meindertkroese <>
2024-07-15 16:58:49 +0200

    so far, not so good...

--- scripts/server.py
+++ scripts/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import os
+from saxonche import PySaxonProcessor, PyXdmValue, PySaxonApiError
 
 app = Flask(__name__)
 
@@ -6,8 +8,16 @@ app = Flask(__name__)
 #  localhost/clarin/cmd/mapping/check?prof=<id>
 def check():
     profile = request.args.get('prof', None)
-    return f"<b>{profile}</b>"
-#    return render_template('index.html', profile=profile)
+
+    with PySaxonProcessor(license=False) as proc:
+        print(f"Processor: {proc.version}")
+        xsltproc = proc.new_xslt30_processor()
+        xsltproc.set_cwd(os.getcwd())
+        executable = xsltproc.compile_stylesheet(stylesheet_file="data/check.xsl")
+        value = proc.make_string_value(profile)
+        executable.set_parameter("prof", value)
+        result = executable.apply_templates_returning_string(source_file="data/index.html")
+    return f"<p>{profile}</p><br/><p>{result}</p>"
 
 @app.route("/")
 def hello_world():
