352b1e3d0bdd433aeb566b3531863ec03b0c8e14
meindertkroese <>
2024-07-16 08:54:29 +0200

    working so far, but ... saxon does not approve...

--- scripts/server.py
+++ scripts/server.py
@@ -1,23 +1,44 @@
-from flask import Flask, render_template, request
 import os
+import ssl
+import urllib.request
+from flask import Flask, render_template, request
 from saxonche import PySaxonProcessor, PyXdmValue, PySaxonApiError
+
 
 app = Flask(__name__)
 
-@app.route("/clarin/cmd/mapping/check", methods=['GET'])
-#  localhost/clarin/cmd/mapping/check?prof=<id>
-def check():
-    profile = request.args.get('prof', None)
 
+@app.route("/clarin/cmd/mapping",)
+def index():
+    return render_template("index.html")
+
+@app.route("/clarin/cmd/mapping/check", methods=['GET'])
+def check():
+    # dirty trick to avoid "urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] ... ":
+    context = ssl._create_unverified_context()
+    # profile
+    prof = request.args.get('prof', None)
+    p_url = f"https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/{prof}/xml"
+    fp = urllib.request.urlopen(p_url,context=context)
+    prof_result = fp.read()
+    profile = prof_result.decode("utf8")
+    fp.close()
+    # facet
+    f_url = "https://raw.githubusercontent.com/clarin-eric/VLO-mapping/master/mapping/facetConcepts.xml"
+    fp = urllib.request.urlopen(f_url,context=context)
+    facet_result = fp.read()
+    facet = facet_result.decode("utf8")
+    fp.close()
+    #
     with PySaxonProcessor(license=False) as proc:
         print(f"Processor: {proc.version}")
         xsltproc = proc.new_xslt30_processor()
         xsltproc.set_cwd(os.getcwd())
         executable = xsltproc.compile_stylesheet(stylesheet_file="data/check.xsl")
-        value = proc.make_string_value(profile)
-        executable.set_parameter("prof", value)
-        result = executable.apply_templates_returning_string(source_file="data/index.html")
-    return f"<p>{profile}</p><br/><p>{result}</p>"
+        config = proc.make_string_value(facet)
+        executable.set_parameter("conf", config)
+        result = executable.apply_templates_returning_string(source_file=profile)
+    return result
 
 @app.route("/")
 def hello_world():
